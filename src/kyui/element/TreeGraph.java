f865a1ff55beb64b127cf1b372c4c55ff71fe00a
EX867 <karnosex867@gmail.com>
2017-11-23 20:03:34 +0900

    트리뷰 유아이 끝낸겸 커밋.나중에 뷰를 보여주는걸 할땐 뷰 각각에 대해 Element단에서 조작이 필요함.

--- src/kyui/element/TreeGraph.java
+++ src/kyui/element/TreeGraph.java
@@ -1,19 +1,20 @@
 package kyui.element;
 import kyui.core.Element;
 import kyui.core.KyUI;
-import kyui.event.listeners.MouseEventListener;
 import kyui.util.Rect;
 import processing.core.PGraphics;
 import processing.event.MouseEvent;
 
 import java.util.ArrayList;
+import java.util.Stack;
 public class TreeGraph extends Element {//includes scroll. for now, I only need horizontal one.
-  NodeI root;
-  ArrayList<NodeI> nodes;
-  NodeI selection;
+  Node root;
+  ArrayList<Node> nodes;
+  Node selection;
   //modifiable values
   public int selectionColor;
   public int strokeWidth=2;
+  public int linkWidth=6;
   public float intervalX=20;
   public float intervalY=20;
   //
@@ -22,7 +23,6 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
   //temp vars
   int count=0;
   int maxDepth=0;
-  int[] depthCount;
   private float clickOffsetX=0;
   private float clickOffsetY=0;
   private float childrenSizeX=0;
@@ -30,6 +30,7 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
   private float clickScrollMaxSq=0;
   float selectionOffsetX=0;
   float selectionOffsetY=0;
+  boolean selectionControl;
   Rect defaultRootPos=new Rect(-60, -30, 60, 30);
   public TreeGraph(String name, String rootText) {
     super(name);
@@ -41,9 +42,9 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
     init(rootText);
   }
   private void init(String rootText) {
-    nodes=new ArrayList<NodeI>();
+    nodes=new ArrayList<Node>();
     selection=null;
-    root=new NodeI(getName() + ":root", 0, this);
+    root=new Node(getName() + ":root", 0, this);
     root.pos=(defaultRootPos);
     root.text=rootText;
     nodes.add(root);
@@ -60,30 +61,38 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
   @Override
   public void onLayout() {
     maxDepth=0;
-    for (NodeI n : nodes) {//first calculate depth.
+    for (Node n : nodes) {//first calculate depth.
       if (maxDepth < n.depth) {
         maxDepth=n.depth;
       }
+      n.index=0;
     }
-    int maxCount=0;
-    float centerX=(pos.left + pos.right) / 2;
-    float centerY=(pos.top + pos.bottom) / 2;
-    depthCount=new int[maxDepth + 1];
-    for (NodeI n : nodes) {//then calculate count.
-      n.index=depthCount[n.depth];
-      depthCount[n.depth]++;
-      if (maxCount < depthCount[n.depth]) {
-        maxCount=depthCount[n.depth];
-      }
-    }
+    float centerX=(pos.left + pos.right) / 2 - offsetX;
+    float centerY=(pos.top + pos.bottom) / 2 - offsetY;
     float width=(root.pos.right - root.pos.left);
     float height=(root.pos.bottom - root.pos.top);
-    for (NodeI n : nodes) {//now update position by formula...
-      float top=-(depthCount[n.depth] - 1) * (height + intervalY) / 2 + n.index * (height + intervalY);
-      n.pos.set((width + intervalX) * n.depth + centerX - offsetX, top + centerY - offsetY, (width + intervalX) * n.depth + width + centerX - offsetX, top + height + centerY - offsetY);//not setPosition because will layout other elements later.
+    Stack<Node> stack=new Stack<Node>();
+    stack.push(root);
+    root.pos.set(centerX - width / 2, -height / 2 + centerY, width / 2 + centerX, height / 2 + centerY);
+    float top=0;
+    while (!stack.isEmpty()) {
+      while (stack.peek().index < stack.peek().localNodes.size()) {
+        Node n=stack.peek().localNodes.get(stack.peek().index);
+        stack.peek().index++;
+        stack.push(n);
+        if (n.related) {
+          n.pos.set((width + intervalX) * n.depth - width / 2 + centerX + selectionOffsetX, top - height / 2 + centerY + selectionOffsetY, (width + intervalX) * n.depth + width / 2 + centerX + selectionOffsetX, top + height / 2 + centerY + selectionOffsetY);//not setPosition because will layout other elements later.
+        } else {
+          n.pos.set((width + intervalX) * n.depth - width / 2 + centerX, top - height / 2 + centerY, (width + intervalX) * n.depth + width / 2 + centerX, top + height / 2 + centerY);
+        }
+      }
+      top+=(height + intervalY);
+      while (stack.size() > 0 && stack.peek().index >= stack.peek().localNodes.size()) {
+        stack.pop();
+      }
     }
-    childrenSizeX=(maxDepth - 1) * width;
-    childrenSizeY=(maxCount - 1) * height;
+    childrenSizeX=maxDepth * (width + intervalX);
+    childrenSizeY=top - (height + intervalY);
     root.onLayout();//this will layout elements except nodes.
     setOffset(offsetX, offsetY);//range check
     invalidate();
@@ -95,12 +104,12 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
   @Override
   public void render(PGraphics g) {
     super.render(g);
-    //KyUI.clipRect(g, pos);
+    KyUI.clipRect(g, pos);
     //ADD>> draw grid
   }
   @Override
   public void overlay(PGraphics g) {
-    //KyUI.removeClip(g);
+    KyUI.removeClip(g);
   }
   //offset 0 is root in center.
   public void setOffset(float valueX, float valueY) {
@@ -113,12 +122,22 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
     if (offsetX < 0) {
       offsetX=0;
     }
-    if (offsetY > childrenSizeY / 2) {//y is different because TreeGraph is aligned LEFT,CENTER.
-      offsetY=childrenSizeY / 2;
+    if (offsetY > childrenSizeY) {
+      offsetY=childrenSizeY;
     }
-    if (offsetY < -childrenSizeY / 2) {
-      offsetY=-childrenSizeY / 2;
+    if (offsetY < 0) {
+      offsetY=0;
     }
+  }
+  @Override
+  public boolean mouseEvent(MouseEvent e, int index) {
+    if (e.getAction() == MouseEvent.RELEASE) {
+      if (selection != null) {
+        selection.unselect();
+        return false;
+      }
+    }
+    return true;
   }
   @Override
   public boolean mouseEventIntercept(MouseEvent e) {//from LinearLayout.
@@ -130,87 +149,91 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
       if (pressedL) {
         requestFocus();
         float valueX=(KyUI.mouseClick.x - KyUI.mouseGlobal.x) * KyUI.scaleGlobal;
-        float valueY=(KyUI.mouseClick.y - KyUI.mouseGlobal.y) * KyUI.scaleGlobal;
+        float valueY=-(KyUI.mouseClick.y - KyUI.mouseGlobal.y) * KyUI.scaleGlobal;
         float value=valueX * valueX + valueY * valueY;
         clickScrollMaxSq=Math.max(value, clickScrollMaxSq);
-        if (selection != null && pos.contains(KyUI.mouseGlobal.x, KyUI.mouseGlobal.y)) {
-          selectionOffsetX=valueX;
+        if (selection != null) {
+          selectionControl=(selection.pos.contains(KyUI.mouseGlobal.x, KyUI.mouseGlobal.y) || selectionControl);
+        }
+        if (selection != null && selectionControl) {
+          selectionOffsetX=-valueX;//????
           selectionOffsetY=valueY;
           onLayout();
           return false;
         } else {
           setOffset(clickOffsetX + valueX, clickOffsetY - valueY);
           localLayout();
-        }
-        if (clickScrollMaxSq > KyUI.GESTURE_THRESHOLD * KyUI.GESTURE_THRESHOLD) {
-          return false;
-        } else {
-          offsetX=clickOffsetX;
-          offsetY=clickOffsetY;
+          if (clickScrollMaxSq > KyUI.GESTURE_THRESHOLD * KyUI.GESTURE_THRESHOLD) {
+            return false;
+          } else {
+            offsetX=clickOffsetX;
+            offsetY=clickOffsetY;
+          }
         }
       }
     } else if (e.getAction() == MouseEvent.RELEASE) {
-      selectionOffsetX=0;
-      selectionOffsetY=0;
       if (selection != null) {
-        for (NodeI n : nodes) {
-          if (n != selection && n.pos.contains(KyUI.mouseGlobal.x, KyUI.mouseGlobal.y)) {
-            selection.parent.removeNode(selection);
-            root.addNode(selection);
+        selectionOffsetX=0;
+        selectionOffsetY=0;
+        for (Node n : nodes) {
+          selectionControl=(selection.pos.contains(KyUI.mouseGlobal.x, KyUI.mouseGlobal.y) || selectionControl);
+          if (n != selection && n.pos.contains(KyUI.mouseGlobal.x, KyUI.mouseGlobal.y) && selectionControl) {//move node
+            Node s=selection;
+            selection.unselect();
+            s.parent.removeNode(s);
+            n.addNode(s);
+            s.setDepth(n.depth + 1);
+            onLayout();
+            invalidate();
+            selectionControl=false;
             return false;
           }
         }
+        onLayout();
+        invalidate();
+        selectionControl=false;
       } else if (pressedL && clickScrollMaxSq > KyUI.GESTURE_THRESHOLD * KyUI.GESTURE_THRESHOLD) {
         return false;
       }
     }
     return true;
   }
-  public static interface Node {
-    public Node addNode(String text);
-    public Node get(int index);
-    public void removeNode(Node node);
-  }
-  public class NodeI extends Button implements Node {
+  public static class Node extends Button {
     TreeGraph Ref;
-    NodeI parent;
-    ArrayList<NodeI> localNodes;//because children not only includes nodes. added node using addNode() puts new node into this.
+    Node parent;
+    ArrayList<Node> localNodes;//because children not only includes nodes. added node using addNode() puts new node into this.
     int depth;
     boolean related=false;//if related, selection's position will be added when layout.
     int index;//temp var when used in layout.
-    public NodeI(String name, int depth_, TreeGraph Ref_) {
+    public Node(String name, int depth_, TreeGraph Ref_) {
       super(name);
       depth=depth_;
       Ref=Ref_;
-      localNodes=new ArrayList<NodeI>();
+      localNodes=new ArrayList<Node>();
     }
     public boolean selected=false;
-    @Override
     public Node addNode(String text) {
-      NodeI n=new NodeI(Ref.getName() + ":" + count, depth + 1, Ref);
+      Node n=new Node(Ref.getName() + ":" + Ref.count, depth + 1, Ref);
       n.parent=this;
       n.text=text;
-      n.setPressListener(new NodeClickListener(n));
       localNodes.add(n);
       Ref.nodes.add(n);
+      Ref.count++;
       Ref.addChild(n);
-      count++;
       return n;
     }
-    protected Node addNode(NodeI n) {//no child adding
-      n.text=text;
+    protected Node addNode(Node n) {//no child adding and create listener
+      if (n == this) {
+        return n;
+      }
       n.parent=this;
-      n.setPressListener(new NodeClickListener(n));
       localNodes.add(n);
       Ref.nodes.add(n);
-      count++;
       return n;
     }
-    @Override
     public void removeNode(Node node) {
       localNodes.remove(localNodes.indexOf(node));
     }
-    @Override
     public Node get(int index) {
       return localNodes.get(index);
     }
@@ -218,20 +241,29 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
     public void render(PGraphics g) {
       super.render(g);
       g.noFill();
-      g.stroke(Ref.bgColor);//FIX>>to selectionColor
-      g.strokeWeight(strokeWidth);
       if (selected) {
-        pos.render(g, -strokeWidth / 2);
+        g.stroke(0);//FIX>>to selectionColor
+        g.strokeWeight(6);//FIX>>to strokeWidth
+        pos.render(g, -Ref.strokeWidth / 2);
+      }
+      if (this != Ref.root) {
+        g.strokeWeight(Ref.linkWidth);
+        g.stroke(bgColor);//FIX>>to selectionColor
+        float xdist=Math.abs(pos.left - parent.pos.right); //* 3 / 4;//from kpm...
+        g.bezier(pos.left, (pos.top + pos.bottom) / 2, pos.left - xdist, (pos.top + pos.bottom) / 2, parent.pos.right + xdist, (parent.pos.top + parent.pos.bottom) / 2, parent.pos.right, (parent.pos.top + parent.pos.bottom) / 2);
       }
       g.noStroke();
     }
     void select() {
+      if (Ref.root == this) {
+        return;
+      }
       selected=true;
       select_();
-      selection=this;
+      Ref.selection=this;
     }
     private void select_() {
-      for (NodeI child : localNodes) {//recursive select.
+      for (Node child : localNodes) {//recursive select.
         child.select_();
       }
       related=true;
@@ -239,13 +271,19 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
     void unselect() {
       selected=false;
       unselect_();
-      selection=null;
+      Ref.selection=null;
     }
     private void unselect_() {
-      for (NodeI child : localNodes) {//recursive select.
+      for (Node child : localNodes) {//recursive select.
         child.unselect_();
       }
       related=false;
+    }
+    void setDepth(int depth_) {
+      depth=depth_;
+      for (Node child : localNodes) {
+        child.setDepth(depth + 1);
+      }
     }
     @Override
     public void onLayout() {
@@ -253,17 +291,14 @@ public class TreeGraph extends Element {//includes scroll. for now, I only need 
         e.setPosition(new Rect(pos.left + e.margin, pos.top + e.margin, pos.right - e.margin, pos.bottom - e.margin));
       }
     }
-    class NodeClickListener implements MouseEventListener {
-      NodeI n;
-      public NodeClickListener(NodeI n_) {
-        n=n_;
+    @Override
+    public boolean mouseEvent(MouseEvent e, int index) {
+      if (e.getAction() == MouseEvent.PRESS) {//select node, and set selection's children node to "not related"
+        if (Ref.selection != null) Ref.selection.unselect();
+        select();
+        return true;
       }
-      @Override
-      public boolean onEvent(MouseEvent e, int index) {//select node, and set selection's children node to "not related"
-        if (selection != null) selection.unselect();
-        n.select();
-        return false;
-      }
+      return super.mouseEvent(e, index);
     }
   }
 }
